4f467177c75c81f73e0baeaccd6eba4408a81f72
Sergio Rosa <scarlosjazz@gmail.com>
2020-06-22 11:27:35 -0400

    add: routes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const routes = require('./src/routes');
+
+const routes = express.Router;
 
 const server = express();
-const videos = require('./data');
 
 // styles
 server.use(express.static('public'));
@@ -12,25 +14,6 @@ nunjucks.configure('views', {
   express: server,
   autoescape: false,
   noCache: true,
-});
-
-server.get('/', function (require, response) {
-  const about = {
-    avatar_url: '/img/sergio.jpg',
-    name: 'Sergio Rosa',
-    role: 'Software Engineer',
-    description:
-      'Full-stack, focus on Node.js, ReactJS, React Native, JavaScript, CSS, HTML',
-    links: [
-      { name: 'Github', url: 'https://github.com/srosajazz' },
-      { name: 'LinkedIn', url: 'https://www.linkedin.com/in/sergiorosa1/' },
-    ],
-  };
-  return response.render('about', { about });
-});
-
-server.get('/portfolio', function (require, response) {
-  return response.render('portfolio', { items: videos });
 });
 
 server.listen(5000, function () {
